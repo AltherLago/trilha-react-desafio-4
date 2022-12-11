b956b42fe4fd27d32caf1dc6ad282ab12eb5e641
AltherLago <ajsl@ic.ufal.br>
2022-12-11 19:57:05 -0300

    Validação de senha

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/Login";
-
-import GlobalStyles from "./global";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
